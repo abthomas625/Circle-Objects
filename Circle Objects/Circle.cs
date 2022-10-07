99823ee3fb9082f90a1b8ec6d9ff927d9a26e172
Abilena <abthomas625@gmail.com>
2022-10-07 12:48:32 -0500

    First commit

---
+++ Circle Objects/Circle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circle_Objects
+{
+    internal class Circle
+    {
+        public double Radius { get; set; }
+        //public double CirclesMade { get; set; } = 0;
+
+        public Circle(double Radius)
+        {
+            this.Radius = Radius;
+        }
+        //circumference = 2*PI*radius
+        public double CalculateCircumference(double radius)
+        {
+            double circumference = 2 * Math.PI * radius;
+            return Math.Round(circumference, 2);
+        }
+        //area = PI*radius squared
+        public double CalculateArea(double radius)
+        {
+            double area = Math.PI * (radius * radius);
+            return Math.Round(area, 2);
+        }
+    }
+
+    
+}
+
+
---
+++ Circle Objects/Program.cs
@@ -0,0 +1,65 @@
+﻿using Circle_Objects;
+
+namespace Circle_Objects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Initial greeting for user
+            Console.WriteLine("Welcome to the Circle Object Generator!");
+
+            bool goOn = true;
+            while (goOn == true)
+            {
+                //Ask user to enter radius
+                Console.WriteLine("Enter a radius");
+                double radius = double.Parse(Console.ReadLine());
+                //Convert user input to a new object
+                Circle c = new Circle(radius);
+
+                //Validate user input is a positive integer
+                if(radius > 0)
+                {
+                    //Calculate and display circumference and area using methods created in Class Circle
+                    c.CalculateCircumference(radius);
+                    Console.WriteLine($"Circumference: {c.CalculateCircumference(radius)}");
+                    c.CalculateArea(radius);
+                    Console.WriteLine($"Area: {c.CalculateArea(radius)}");
+                }
+                else
+                {
+                    Console.WriteLine("Please enter a number with a positive value.");
+                }
+                goOn = AskToContinue();
+            }
+        }
+        static bool AskToContinue()
+        {
+            //Variable created to count number of times user creates a circle
+            int circleCount = 1;
+            circleCount++;
+
+            Console.WriteLine("Would you like to try again? Y/N");
+            string response = Console.ReadLine().ToLower();
+
+            if (response == "y")
+            {                
+                return true;
+            }
+            else if(response == "n")
+            {
+                //Return the number of circle objects created with goodbye message
+                Console.WriteLine($"Circles made: {circleCount} \nThank you for using the Circle Object Generator. Goodbye!");
+                return false;
+            }
+            else
+            {
+                Console.WriteLine("I'm sorry. I didn't understand that. Please, try again.");
+                return AskToContinue();
+            }
+        }
+    }
+}
+
+
